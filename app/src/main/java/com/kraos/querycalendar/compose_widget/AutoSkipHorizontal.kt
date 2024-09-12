0009bb7f169304ef1639b48867a3a809bd277731
kraos <980737054@qq.com>
2024-09-12 17:08:22 +0800

    feat:新增懒加载的自动缩略布局

--- app/src/main/java/com/kraos/querycalendar/compose_widget/AutoSkipHorizontal.kt
+++ app/src/main/java/com/kraos/querycalendar/compose_widget/AutoSkipHorizontal.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -107,6 +108,81 @@ fun AutoSkipHorizontal(
 }
 
 @Composable
+fun LazyAutoSkipHorizontal(
+    modifier: Modifier = Modifier,
+    skipIndicator: @Composable () -> Unit,
+    verticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
+    horizontalSpacing: Dp = 0.dp,
+    content: List<@Composable () -> Unit>,
+) {
+
+    SubcomposeLayout(modifier = modifier) { constraints ->
+        val placeables = mutableListOf<Placeable>()
+        var layoutWidth = 0
+        var maxShowItem = 0
+
+        // 逐步测量子项，直到填满布局宽度
+        for (item in content) {
+            val placeable = subcompose(Slot.Content) { item() }
+                .map { it.measure(constraints.copy(minWidth = 0)) }
+                .first()
+
+            if (layoutWidth + placeable.width + horizontalSpacing.roundToPx() > constraints.maxWidth) {
+                break
+            }
+
+            if (maxShowItem != 0) {
+                layoutWidth += horizontalSpacing.roundToPx()
+            }
+
+            layoutWidth += placeable.width
+            placeables.add(placeable)
+            maxShowItem++
+        }
+
+        // 测量指示器
+        val indicatorPlaceable = subcompose(Slot.Indicator, skipIndicator)
+            .map { it.measure(constraints.copy(minWidth = 0)) }
+            .firstOrNull() ?: error("没有指示器")
+
+        if (maxShowItem < content.size) {
+            layoutWidth += horizontalSpacing.roundToPx() + indicatorPlaceable.width
+        }
+
+        val layoutHeight = if (constraints.hasFixedHeight) {
+            constraints.maxHeight
+        } else {
+            placeables.maxOfOrNull { it.height } ?: 0
+        }
+
+        // 布局子项和指示器
+        layout(
+            width = if (constraints.hasFixedWidth) constraints.maxWidth else layoutWidth,
+            height = layoutHeight
+        ) {
+            var left = 0
+
+            placeables.forEachIndexed { index, placeable ->
+                if (index != 0) left += horizontalSpacing.roundToPx()
+                placeable.placeRelative(
+                    x = left,
+                    y = verticalAlignment.align(size = placeable.height, space = layoutHeight)
+                )
+                left += placeable.width
+            }
+
+            if (maxShowItem < content.size) {
+                left += horizontalSpacing.roundToPx()
+                indicatorPlaceable.placeRelative(
+                    x = left,
+                    y = verticalAlignment.align(size = indicatorPlaceable.height, space = layoutHeight)
+                )
+            }
+        }
+    }
+}
+
+@Composable
 @Preview
 private fun PreviewAutoSkipHorizontal() {
 
@@ -122,6 +198,35 @@ private fun PreviewAutoSkipHorizontal() {
         horizontalSpacing = 10.dp
     ) {
         (0..8).forEach {
+            Box(
+                Modifier
+                    .size(60.dp, 20.dp)
+                    .background(Color.Green),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("$it")
+            }
+        }
+    }
+
+}
+
+@Composable
+@Preview
+private fun PreviewLazyAutoSkipHorizontal() {
+
+    AutoSkipHorizontal(
+        modifier = Modifier.fillMaxWidth(),
+        skipIndicator = {
+            Box(
+                Modifier
+                    .size(20.dp, 60.dp)
+                    .background(Color.Red)
+            )
+        },
+        horizontalSpacing = 10.dp
+    ) {
+        (0..5).forEach {
             Box(
                 Modifier
                     .size(60.dp, 20.dp)
