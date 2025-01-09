9e5cd082bb68ecd20ef3b2dd0bffd68b4e160852
kraos <980737054@qq.com>
2025-01-09 18:19:10 +0800

    opt:代码整理

--- app/src/main/java/com/kraos/querycalendar/activity/TestCalenderHistoryActivity.kt
+++ app/src/main/java/com/kraos/querycalendar/activity/TestCalenderHistoryActivity.kt
@@ -2,27 +2,16 @@ package com.kraos.querycalendar.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.kraos.querycalendar.compose_widget.BeyondPagerIndicator
 import com.kraos.querycalendar.compose_widget.CalendarHistory
 import com.kraos.querycalendar.compose_widget.CalendarHistory.DateConfigState
-import com.kraos.querycalendar.compose_widget.ZoomPager
-import java.text.SimpleDateFormat
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Calendar
 import java.util.Date
-import java.util.Locale
 
 /**
  * 一个仿QQ聊天记录时间线的日历
