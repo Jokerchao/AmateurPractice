68e6cf76a8e4a05c648b168badf181187f912777
Kraos <980737054@qq.com>
2025-03-23 00:15:49 +0800

    feat:新增TransferMode的练习

--- app/src/main/java/com/kraos/querycalendar/view/TransModeView.kt
+++ app/src/main/java/com/kraos/querycalendar/view/TransModeView.kt
@@ -1,6 +1,8 @@
 package com.kraos.querycalendar.view
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
@@ -11,6 +13,7 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.core.graphics.createBitmap
 import androidx.core.graphics.toColorInt
+import com.kraos.querycalendar.R
 import com.kraos.querycalendar.util.px
 
 /**
@@ -25,8 +28,7 @@ class TransModeView @JvmOverloads constructor(
     private val paint = Paint(ANTI_ALIAS_FLAG)
     private val circleBitmap = createBitmap(150f.px.toInt(), 150f.px.toInt())
     private val rectBitmap = createBitmap(150f.px.toInt(), 150f.px.toInt())
-    private val xfermode: Xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-//    private val rectF = RectF(150f.px, 50f.px, 300f.px, 200f.px)
+    private val xfermode: Xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP)
 
     init {
         val canvas = Canvas(circleBitmap)
@@ -38,16 +40,31 @@ class TransModeView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
+        //切头像
+//        val count = canvas.saveLayer(0f.px, 0f.px, 300f.px, 300f.px, null)
+//        canvas.drawOval(150f.px, 50f.px, 300f.px, 200f.px, paint)
+//        paint.xfermode = xfermode
+//        canvas.drawBitmap(getAvatar(150f.px.toInt()), 150f.px, 50f.px, paint)
+//        paint.xfermode = null
+//        canvas.restoreToCount(count)
+
+        //官网的xferMode展示
         val count = canvas.saveLayer(0f.px, 0f.px, 300f.px, 300f.px, null)
         canvas.drawBitmap(circleBitmap, 150f.px, 50f.px, paint)
-//        paint.color = "#FF0000".toColorInt()
-//        canvas.drawOval(rectF, paint)
         paint.xfermode = xfermode
         canvas.drawBitmap(rectBitmap, 150f.px, 50f.px, paint)
-//        paint.color = "#00FF00".toColorInt()
-//        canvas.drawRect(50f.px, 150f.px, 200f.px, 300f.px, paint)
         paint.xfermode = null
         canvas.restoreToCount(count)
+    }
+
+    fun getAvatar(width: Int): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+        options.inJustDecodeBounds = false
+        options.inDensity = options.outWidth
+        options.inTargetDensity = width
+        return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
     }
 
 }
